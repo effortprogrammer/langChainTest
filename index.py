9bf8d391537243141895bfbf6532b2a545e31bdb
Hojin Yang <yhojin22@vt.edu>
2023-05-12 00:17:10 -0400

    first commit

---
+++ index.py
@@ -0,0 +1,9 @@
+import openai
+from PyPDF2 import PdfReader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
+import os
+
+os.environ["OPENAI_API_KEY"] = "test"
+
